01f1b616d870a703c13bd1f8951951558aa6e6e6
terahje <terahjequinn@gmail.com>
2021-04-11 22:19:00 -0700

    updated product template to render page per handle

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import { graphql } from 'gatsby';
+
+export const query = graphql`
+  query ProductQuery($shopifyId: String) {
+    shopifyProduct(shopifyId: { eq: $shopifyId }) {
+      title
+    }
+  }
+`;
 
 export default function ProductTemplate(props){
     console.log(props);
-    return <h1>Product</h1>;
+    return <h1>{props.data.shopifyProduct.title}</h1>;
 }
