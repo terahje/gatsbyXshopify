1aab6cdf859a1f348952e9dab2aaf3b05688a17b
terahje <terahjequinn@gmail.com>
2021-04-14 10:49:06 -0700

    added variant options and added price

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { Layout, ImageGallery } from 'components'
-import { Grid } from './styles'
+import { Grid, SelectWrapper, Price } from './styles'
+import CartContext from 'context/CartContext'
 
 export const query = graphql`
   query ProductQuery($shopifyId: String) {
     shopifyProduct(shopifyId: { eq: $shopifyId }) {
+      shopifyId
       title
       description
       images {
@@ -23,11 +25,44 @@ export const query = graphql`
 `;
 
 export default function ProductTemplate(props){
-    return <Layout>
+    const {getProductById} = React.useContext(CartContext)
+    const [product, setProduct] = React.useState(null)
+    const [selectedVariant, setSelectedVariant] = React.useState(null)
+
+    React.useEffect(() =>{
+        getProductById(props.data.shopifyProduct.shopifyId).then(result => {
+            setProduct(result);
+            setSelectedVariant(result.variants[0]);
+        })
+    }, [getProductById, setProduct])
+
+    const handleVariantChange = e => {
+        setSelectedVariant(product?.variants.find(v => v.id === e.target.value))
+    };
+
+    return( 
+    <Layout>
         <Grid>
             <div>
                 <h1>{props.data.shopifyProduct.title}</h1>
                 <p>{props.data.shopifyProduct.description}</p>
+                {product?.availableForSale &&
+                <>
+                <SelectWrapper>
+                    <strong>Variant</strong>
+                    <select onChange={handleVariantChange}>
+                        {product?.variants.map(v => (
+                            <option key={v.id} value={v.id}>
+                                {v.title}</option>
+                        ))}
+                    </select>
+                </SelectWrapper>
+                {!!selectedVariant && 
+                <Price>
+                    ${selectedVariant.price}
+                </Price>}
+                </>
+                }
             </div>
 
             <div>
@@ -35,4 +70,5 @@ export default function ProductTemplate(props){
             </div>
             </Grid>
         </Layout>
+    )
 }
